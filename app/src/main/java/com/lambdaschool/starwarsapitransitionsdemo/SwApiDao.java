184e11385153dc9f71213c3bbdf9311dc6b33110
Jake Esveld <thejakeesveld@gmail.com>
2019-04-22 09:40:56 -0700

    add child class for Star wars person

--- app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/SwApiDao.java
+++ app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/SwApiDao.java
@@ -10,14 +10,20 @@ public class SwApiDao {
     private static final String STARSHIP_URL = BASE_URL + "starships/";
 
     // S04M03-5 write method to get person
-    public static SwApiObject getPerson(int id) {
+    public static Person getPerson(int id) {
         final String result = NetworkAdapter.httpRequest(PERSON_URL + id);
 
-        SwApiObject object = null;
+        Person object = null;
         try {
             JSONObject json = new JSONObject(result);
 
-            object = new SwApiObject(id, json.getString("name"));
+            String name = json.getString("name");
+            String height = json.getString("height");
+            String mass = json.getString("mass");
+            String hairColor = json.getString("hair_color");
+            String skinColor = json.getString("skin_color");
+            String eyeColor = json.getString("eye_color");
+            object = new Person(id, name, height, mass, hairColor, skinColor, eyeColor);
             object.setCategory(DrawableResolver.CHARACTER);
 
         } catch (JSONException e) {
