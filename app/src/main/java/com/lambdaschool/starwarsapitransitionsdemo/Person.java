3713c018dc962f1c82219752a5452e9bbeffad11
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-22 11:26:32 -0500

    added Person vars

--- app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/Person.java
+++ app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/Person.java
@@ -5,4 +5,36 @@ public class Person extends SwApiObject {
     public Person(int id, String name) {
         super(id, name);
     }
+
+    private String height, mass, harcolor, skinColor, eyeColor;
+
+    public Person(int id, String name, String height, String mass, String harcolor, String skinColor, String eyeColor) {
+        super(id, name);
+        this.height = height;
+        this.mass = mass;
+        this.harcolor = harcolor;
+        this.skinColor = skinColor;
+        this.eyeColor = eyeColor;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getMass() {
+        return mass;
+    }
+
+    public String getHarcolor() {
+        return harcolor;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+    
 }
