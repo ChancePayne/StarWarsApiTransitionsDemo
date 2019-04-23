bafb29e6ba48361748eb720dc75cd4c7f485805c
heiligbasil <basilief@yahoo.com>
2019-04-23 09:53:45 -0700

    Added JSONable

--- app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/Person.java
+++ app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/Person.java
@@ -1,6 +1,9 @@
 package com.lambdaschool.starwarsapitransitionsdemo;
 
-public class Person extends SwApiObject {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Person extends SwApiObject implements JSONable{
 
     protected String height, mass, hairColor, skinColor, eyeColor;
 
@@ -40,5 +43,44 @@ public class Person extends SwApiObject {
     @Override
     public String toString() {
         return String.format("%s, is %scm tall, they have %s skin, and %s hair with %s eyes.", this.name, this.height, this.skinColor, this.hairColor, this.eyeColor);
+    }
+
+    @Override
+    public String toJsonString() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("name", this.name);
+
+        json.put("height", this.height);
+        json.put("hair_color", this.hairColor);
+        json.put("skin_color", this.skinColor);
+        json.put("eye_color", this.eyeColor);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @Override
+    public void fromJsonString(String jsonString) {
+        JSONObject json = new JSONObject(result);
+
+        final String name;
+        try {
+            name = json.getString("name");
+
+        final String height = json.getString("height");
+        final String mass = json.getString("mass");
+        final String hairColor = json.getString("hair_color");
+        final String skinColor = json.getString("skin_color");
+        final String eyeColor = json.getString("eye_color");
+        String[] urlParts=json.getString("url").split("/");
+        this.id=Integer.parseInt(urlParts[urlParts.length-2]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        object = new Person(id, name, height, mass, hairColor, skinColor, eyeColor);
     }
 }
